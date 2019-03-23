faf52fa8ffeedc0075aa634f840bb24622bc498c
jeremeykenney <48864715+jeremeykenney@users.noreply.github.com>
2019-03-23 14:29:33 -0400

    Loading code

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Lab01
+// Hello world from app
+
+using System;
+
+class Welcome1
+{
+   //Main method begins execution of C# app
+   static void Main()
+   {
+      string person = "Jeremey"; // variable that stores my name
+      Console.WriteLine($"Hello world from, {person}!");
+   } // end Main
+} // end class Welcome1
